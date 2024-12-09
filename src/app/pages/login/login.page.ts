afec26ca0675e5b0abbd1ed0b42bc1c0cfff9123
Estvexx <162581957+Estvexx@users.noreply.github.com>
2024-12-09 12:57:19 +0000

    form login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private router:Router, private toastController: ToastController) {
+    
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]], 
+      password: ['', [Validators.required]],
+    });
+  }
 
-  ngOnInit() {
+  async onSubmit() {
+    if (this.loginForm.valid) {
+      // Exibir Toast de sucesso
+      const toast = await this.toastController.create({
+        message: 'Login realizado com sucesso!',
+        duration: 2000,
+        color: 'success', // cor do toast
+        position: 'bottom', // posição do toast
+      });
+      toast.present();
+
+      console.log('Formulário enviado com sucesso!', this.loginForm.value);
+    } else {
+      // Exibir Toast de erro
+      const toast = await this.toastController.create({
+        message: 'Por favor, preencha todos os campos corretamente!',
+        duration: 2000,
+        color: 'danger', // cor do toast
+        position: 'bottom',
+      });
+      toast.present();
+
+      console.log('Formulário inválido');
+    }
   }
 
 }
