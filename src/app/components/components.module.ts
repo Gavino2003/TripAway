269b342dbebd23e9406fd2c5149dd1c6dc5ed331
Estvexx <162581957+Estvexx@users.noreply.github.com>
2024-12-16 15:59:36 +0000

    NavBar e footer

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
+import { NavBar2Component } from './nav-bar2/nav-bar2.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [NavBarComponent],
-  imports: [CommonModule, IonicModule, RouterModule],
-  exports: [NavBarComponent],
+  declarations: [NavBarComponent, NavBar2Component, FooterComponent], 
+  imports: [CommonModule, IonicModule, RouterModule,],
+  exports: [NavBarComponent, NavBar2Component, FooterComponent],
 })
 export class ComponentsModule {}
