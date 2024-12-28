5ba4cc215b559ebf4074564213dd8f85cc10916d
Gavino2003 <vascogavino@gmail.com>
2024-12-28 18:16:32 +0000

    botao flutuante

--- src/app/components/navigationbtn/navigationbtn.component.ts
+++ src/app/components/navigationbtn/navigationbtn.component.ts
@@ -6,12 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigationbtn.component.scss'],
 })
 export class NavigationbtnComponent  implements OnInit {
-  fabOpened = false;
-  constructor() { }
+  isDarkMode: boolean = false;
+  constructor() { 
+    this.isDarkMode = document.body.classList.contains('dark');
+  }
 
   ngOnInit() {}
 
-  toggleFab() {
-    this.fabOpened = !this.fabOpened; // Alterna a visibilidade dos botões
+  // Função que alterna entre os modos claro e escuro
+  toggleTheme() {
+    if (this.isDarkMode) {
+      // Se estiver no modo escuro, altera para o modo claro
+      document.body.classList.remove('dark');
+      document.body.classList.add('light');
+    } else {
+      // Se estiver no modo claro, altera para o modo escuro
+      document.body.classList.remove('light');
+      document.body.classList.add('dark');
+    }
+    
+    // Alterna o valor da variável isDarkMode
+    this.isDarkMode = !this.isDarkMode;
   }
 }
